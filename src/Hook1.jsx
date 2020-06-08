dcdf547e562c2cd4341ab4d1da28c9a16d7b0843
PradeepVats <pardeepvats60@gmail.com>
2020-06-08 11:08:16 +0530

    Change in Button

--- src/Hook1.jsx
+++ src/Hook1.jsx
@@ -12,7 +12,7 @@ const Hook1 = () => {
     <>
       <h1 className="heading"> Hook Concept </h1>
       <h1 className="heading"> {count} </h1>
-      <button className="button" onClick={IncNum}>Click & Count</button>
+      <button className="button" onClick={IncNum}>Click and Count</button>
     </>
   );
 };
